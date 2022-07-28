db666be38e4b9752ef59f36182d015c69e39a135
TomMard1337 <tommarwebs@gmail.com>
2022-07-28 23:01:04 +0100

    Fixed account update Added parameter permisions for account update. Now it is possible to freely change account settings. It shows now which gender you are with radio_button so if you are male it will select male vice versa

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ class ApplicationController < ActionController::Base
     protected
 
     def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :username, :date_of_birth) }
-        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :username, :date_of_birth) }
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :username, :date_of_birth, :is_female) }
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:password_confirmation, :current_password, :name, :email, :password, :username, :date_of_birth, :is_female) }
     end
 end
