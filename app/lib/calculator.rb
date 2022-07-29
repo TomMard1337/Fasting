13d1e2359c821f22ba93c2f58ab840927ff965b0
TomMard1337 <tommarwebs@gmail.com>
2022-07-29 01:10:10 +0100

    Removed calculator.rb app/app/lib/calculator.rb Production had errors with this file

--- app/lib/calculator.rb
+++
@@ -1,9 +0,0 @@
-class Body < ApplicationRecord
-  # your other codes 
-  def bmr
-   ((13.397 * self.weight) + 
-     (479.9 * self.height) - 
-     (5.677 * self.age) + 88.362) * 
-     (1 - (self.fat / 100.00))
-  end
- end
