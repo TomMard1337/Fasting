8889633f73b74449cfbcf5b1852be40663fb2826
TomMard1337 <tommarwebs@gmail.com>
2022-08-03 11:33:18 +0100

    Captcha for password reset Added captcha for password reset. It will validate captcha via password controller. Fixed captcha validation text location. It is now above the captcha.

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
   get 'home/contact'
   get 'calculator/bmr'
   devise_for :accounts, controllers: {
-    registrations: 'accounts/registrations', sessions: 'accounts/sessions' }
+    registrations: 'accounts/registrations', sessions: 'accounts/sessions', passwords: 'accounts/passwords' }
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
